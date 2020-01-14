3c54d5f4a94354a31840155d338de56a11588513
Cerestar <javier.violante123@gmail.com>
2020-01-14 00:04:00 -0500

    Deleted template files

--- template/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-	<p class="copyright">
-		© Copyright 2019 Daniel Violante except where noted
-		Privacy Policy | Terms of Service
-		Endless Online © Copyright Vult-r Studios 
-	</p>
-</footer>
--- template/navigation.php
+++
@@ -1,5 +0,0 @@
-<ul class="nav">
-  <li  class="nav"><a class="nav" href="index.php">Home</a></li>
-  <li class="nav"><a class="nav"  href="artist.php">Artist Time</a></li>
-  <li class="nav"><a class="nav"  href="about.php">About</a></li>
-</ul>
