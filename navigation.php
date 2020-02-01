2bb5ab4c36b9bfa14b47cef3aa943bd7cc26feeb
Cerestar <javier.violante123@gmail.com>
2020-02-01 13:34:52 -0500

    Updated Bootstrap Installation

--- navigation.php
+++ navigation.php
@@ -1,19 +1,24 @@
-<ul class="nav">
-<nav class="navbar navbar-expand-sm navbar-dark justify-content-center" style="background-color: #333333;">
-		<ul class="navbar-nav">
-			<li class="nav-item">
-				<a class="nav-link" href="index.php">Home</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="artist.php">Artist Timer</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="about.php">About</a>
-			</li>
-			  <?php
-			    if(isset($_SESSION['userid'])) {
-			      echo '<li style="float:right;"><a class="nav" href="logout.php">Log Out</a></li>';
-			    }
-			  ?>
-		</ul>
-	</nav>
+<nav class="navbar navbar-expand-sm navbar-dark d-flex" style="background-color: #333333;">
+	<ul class="navbar-nav l-auto">
+		<li class="nav-item">
+			<a class="nav-link" href="index.php">Home</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="artist.php">Artist Timer</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="about.php">About</a>
+		</li>
+	</ul>
+	<ul class="navbar-nav ml-auto">
+		<?php
+		    if(isset($_SESSION['userid'])) {
+		      echo '
+		      	<li class="nav-item">
+					<a class="nav-link" href="logout.php">Log Out</a>
+				</li>
+		      ';
+		    }
+		?>
+	</ul>
+</nav>
